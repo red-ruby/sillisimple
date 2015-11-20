5cd2dbf470460b7ccde0dafee06ccedf9c7348d8
red-ruby <danielruby990@yahoo.com>
2015-11-20 13:31:28 +0100

    sillisimple
    sillisimple

---
+++ js/silly.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+
+var SILLY = (function(module){
+	module.Doit= function(resultObject){
+		resultObject.prepend(Date() + '<br/>');
+		};
+	return module;
+	
+	}(SILLY || {}));
